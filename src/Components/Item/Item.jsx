031817d5b1bf9926f481dcc0b359c411d0c4e40d
Monu8406 <124706699+Monu8406@users.noreply.github.com>
2024-03-16 11:55:08 +0530

    staged

---
+++ src/Components/Item/Item.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Item.css';
+import { Link } from 'react-router-dom';
+import { Product } from '../../Pages/Product';
+const Item = (props) => {
+  return (
+    <div className='item'>
+      {localStorage.getItem('auth-token')?<Link to={`product/${props.id}`}> <img  onClick={window.scrollTo(0,0)} src={props.image} alt="" /> </Link>:<Link to="/"> <img  onClick={window.scrollTo(0,0)} src={props.image} alt="" /> </Link>}
+     <p className='name'>{props.name}</p>
+    <div className='item-prices'>
+        <div className="item-price-new">
+           {props.new_price} Rs
+        </div>
+        <div className="item-price-old">
+            {props.old_price} Rs
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Item
