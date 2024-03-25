1f7eea857bd91e15091896bdbd16f5c8cd62ed41
Monu8406 <124706699+Monu8406@users.noreply.github.com>
2024-03-25 08:39:28 +0530

    dnbdbcss

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+
 import './Item.css';
 import { Link } from 'react-router-dom';
-import { Product } from '../../Pages/Product';
+import { Product } from '../../Pages/Product';6
+import cart_icon  from '../Assets/cart_icon.png'
 const Item = (props) => {
   return (
     <div className='item'>
-      {localStorage.getItem('auth-token')?<Link to={`product/${props.id}`}> <img className='img' onClick={window.scrollTo(0,0)} src={props.image} alt="" /> </Link>:<Link to="/"> <img  className='img' onClick={window.scrollTo(0,0)} src={props.image} alt="" /> </Link>}
+      {localStorage.getItem('auth-token')?<Link to={`product/${props.id}`}> <img className='img' onClick={window.scrollTo(0,0)} src="product_1710680889821.png" alt="" /> </Link>:<Link to="/"> <img  className='img' onClick={window.scrollTo(0,0)}  src="product_1710680889821.png"  alt="" /> </Link>}
      <p className='name'>{props.name}</p>
     <div className='item-prices'>
         <div className="item-price-new">
